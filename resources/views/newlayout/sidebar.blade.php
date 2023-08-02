4a732a50928cc4a38379d35da406c8aa20c39ff7
bellasoniadwi <bellasoniadwi@gmail.com>
2023-08-02 11:52:52 +0700

    menampilkan akun di sidebar

--- resources/views/newlayout/sidebar.blade.php
+++ resources/views/newlayout/sidebar.blade.php
@@ -3,7 +3,18 @@
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
         <img src="{{ asset('assets/img/logo-ct.png')}}" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold text-white">Proyek Firebase</span>
+        <span class="ms-1 font-weight-bold text-white">
+          <?php
+                // Mendapatkan informasi akun dari autentikasi Firebase
+                $user = auth()->user(); // Ubah sesuai dengan cara Anda mendapatkan informasi akun
+
+                if ($user) {
+                    echo $user->email; // Ubah sesuai dengan atribut nama akun pada objek $user
+                } else {
+                    echo "Proyek Firebase";
+                }
+            ?>
+        </span>
       </a>
     </div>
     <hr class="horizontal light mt-0 mb-2">
