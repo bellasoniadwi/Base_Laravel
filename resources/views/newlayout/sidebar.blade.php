dd58528be542172acb696d6577a372e02fdaf6ca
bellasoniadwi <bellasoniadwi@gmail.com>
2023-08-02 13:41:36 +0700

    update route di sidebar

--- resources/views/newlayout/sidebar.blade.php
+++ resources/views/newlayout/sidebar.blade.php
@@ -45,7 +45,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white @yield('rekap')" href="../pages/billing.html">
+          <a class="nav-link text-white @yield('rekap')" href="{{route('rekap')}}">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">receipt_long</i>
             </div>
