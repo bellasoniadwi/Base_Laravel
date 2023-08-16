63f249ca946c052b10d2d09b99d7e02880050ef0
bellasoniadwi <bellasoniadwi@gmail.com>
2023-08-16 10:49:43 +0700

    update expired token image firebase

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -202,7 +202,7 @@ class StudentController extends Controller
                     ['name' => $storagePath]
                 );
 
-                $imagePath = $storage->getBucket()->object($storagePath)->signedUrl(now()->addHour());
+                $imagePath = $storage->getBucket()->object($storagePath)->signedUrl(now()->addYears(10));
             } else {
                 $imagePath = null; // If no image is uploaded, set the image path to null
             }
@@ -247,7 +247,7 @@ class StudentController extends Controller
                     ['name' => $storagePath]
                 );
 
-                $imagePath = $storage->getBucket()->object($storagePath)->signedUrl(now()->addHour());
+                $imagePath = $storage->getBucket()->object($storagePath)->signedUrl(now()->addYears(10));
             } else {
                 $firestore = app(Firestore::class);
                 $studentRef = $firestore->database()->collection('students')->document($documentId)->snapshot();
